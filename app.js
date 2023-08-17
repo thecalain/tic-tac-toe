f2e8ac2f4601fec57362ee97885dd7388372db7b
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-08-18 07:55:59 +0800

    Inserts X or O when square is presseed and disables repetition of button from being pressed too

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const square = document.getElementsByClassName("square");
+const board = ["", "", "", "", "", "", "", "", ""];
 
+let count = 0;
 
-square.addEventListener("click", () => {
-  console.log("square");  
-})
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (count % 2 === 0) {
+      square.textContent = "X";
+    } else {
+      square.textContent = "O";
+    }
+    count++;
+    square.setAttribute("disabled", true);
+  });
+});
