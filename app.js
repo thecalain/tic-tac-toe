d0d1aa21e4926c3dcd8099efe28ef5c82b947779
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-08-20 14:39:36 +0800

    checkWin() checks but only through console

--- app.js
+++ app.js
@@ -14,6 +14,55 @@ squares.forEach((square, index) => {
     count++;
     square.setAttribute("disabled", true);
     console.log(board);
+    checkWin(board);
   });
 });
 
+function checkWin(board) {
+  if (board[0] === board[1] && board[1] === board[2] && board[0] !== "") {
+    squares.style
+    console.log("Win!");
+  } else if (
+    board[3] === board[4] &&
+    board[4] === board[5] &&
+    board[3] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[6] === board[7] &&
+    board[7] === board[8] &&
+    board[6] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[0] === board[3] &&
+    board[3] === board[6] &&
+    board[0] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[1] === board[4] &&
+    board[1] === board[4] &&
+    board[1] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[2] === board[5] &&
+    board[2] === board[5] &&
+    board[2] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[0] === board[4] &&
+    board[4] === board[8] &&
+    board[0] !== ""
+  ) {
+    console.log("Win!");
+  } else if (
+    board[2] === board[4] &&
+    board[4] === board[6] &&
+    board[2] !== ""
+  ) {
+    console.log("Win!");
+  }
+}
