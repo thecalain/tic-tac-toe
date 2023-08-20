3ad997255f12d43fe3cd85c2c06bdec42005a9c1
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-08-20 14:02:43 +0800

    puts X and O in board array

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const board = ["", "", "", "", "", "", "", "", ""];
-
+let board = ["", "", "", "", "", "", "", "", ""];
 let count = 0;
 
 const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
+squares.forEach((square, index) => {
   square.addEventListener("click", () => {
     if (count % 2 === 0) {
       square.textContent = "X";
+      board[index] = "X";
     } else {
       square.textContent = "O";
+      board[index] = "O";
     }
     count++;
     square.setAttribute("disabled", true);
+    console.log(board);
   });
 });
+
